1260508e9b4b70563149c5e57e3d05113bc810cf
Bryan García <bryan,garcia08@outlook.com>
2024-07-11 11:58:35 -0600

    Ejercico Java

---
+++ src/ejercicogit1/Ejercicogit1.java
@@ -0,0 +1,14 @@
+
+package ejercicogit1;
+
+
+public class Ejercicogit1 {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <=10; i++) {
+            System.out.println(10 + "*" + i + "=" + 10*i);
+        }
+    }
+    
+}
